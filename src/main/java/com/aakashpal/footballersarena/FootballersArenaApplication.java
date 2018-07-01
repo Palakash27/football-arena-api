83a4f69b6b6940a7549e04c3f4dffb1bd1aa94cd
Aakash Pal <palakash27@gmail.com>
2018-07-01 13:45:04 +0530

    Initial Commit

---
+++ src/main/java/com/aakashpal/footballersarena/FootballersArenaApplication.java
@@ -0,0 +1,12 @@
+package com.aakashpal.footballersarena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballersArenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballersArenaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/aakashpal/footballersarena/FootballersArenaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.aakashpal.footballersarena;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FootballersArenaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
